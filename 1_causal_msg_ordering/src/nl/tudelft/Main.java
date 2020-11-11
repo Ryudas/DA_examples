c1245815aca4787be6598d8b5e508b0466ef7ae8
Ryudas <sivlesegrob9@gmail.com>
2020-11-11 19:06:22 +0100

    Initial RMI structure for message passing

--- 1_causal_msg_ordering/src/nl/tudelft/Main.java
+++ 1_causal_msg_ordering/src/nl/tudelft/Main.java
@@ -1,8 +1,18 @@
 package nl.tudelft;
 
+import java.rmi.RemoteException;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.print("LALA");
+
+        // Initialize registry
+        try {
+            java.rmi.registry.LocateRegistry.createRegistry(1099);
+        } catch (RemoteException e ) {
+            e.printStackTrace();
+        }
     }
 }
