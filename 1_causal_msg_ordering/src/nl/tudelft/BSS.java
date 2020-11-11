76e5533ab7e00bc84726c549dec180d3db4eefed
Ryudas <sivlesegro9@gmail.com>
2020-11-11 19:21:15 +0100

    Added DA process basics

--- 1_causal_msg_ordering/src/nl/tudelft/BSS.java
+++ 1_causal_msg_ordering/src/nl/tudelft/BSS.java
@@ -1,11 +1,20 @@
 package nl.tudelft;
 
+import java.net.MalformedURLException;
+import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 // Birman-Schiper-Stephenson algorithm class
 public class BSS extends UnicastRemoteObject implements  BSS_RMI , Runnable  {
-    protected BSS() throws RemoteException {
+    private String id;
+
+    // constructor for a component of BSS
+    protected BSS(String id) throws RemoteException, AlreadyBoundException, MalformedURLException {
+        // create a BSS component with a certain id and bind it to the remote
+        this.id = id;
+
+        java.rmi.Naming.bind("rmi://localhost/BSS" + id.toString(), this);
     }
 
     @Override
@@ -14,7 +23,7 @@ public class BSS extends UnicastRemoteObject implements  BSS_RMI , Runnable  {
     }
 
     @Override
-    public void send_message() throws RemoteException {
-
+    public void send_message(String text) throws RemoteException {
+        System.out.print(text);
     }
 }
