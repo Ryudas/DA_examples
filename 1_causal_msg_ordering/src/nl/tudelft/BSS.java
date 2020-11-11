7f170fab0fe79cfdf0411fd1924d0700689fb627
Ryudas <sivlesegrob9@gmail.com>
2020-11-11 19:47:11 +0100

    test message passing using RMI

--- 1_causal_msg_ordering/src/nl/tudelft/BSS.java
+++ 1_causal_msg_ordering/src/nl/tudelft/BSS.java
@@ -2,6 +2,7 @@ package nl.tudelft;
 
 import java.net.MalformedURLException;
 import java.rmi.AlreadyBoundException;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
@@ -14,16 +15,35 @@ public class BSS extends UnicastRemoteObject implements  BSS_RMI , Runnable  {
         // create a BSS component with a certain id and bind it to the remote
         this.id = id;
 
-        java.rmi.Naming.bind("rmi://localhost/BSS" + id.toString(), this);
+        java.rmi.Naming.bind("rmi://localhost/BSS_" + id.toString(), this);
     }
 
     @Override
     public void run() {
+        // what does each node do?
+        // send message to node X
+        try {
 
+            BSS node_1 = (BSS) java.rmi.Naming.lookup("rmi://localhost/BSS_" + "1");
+            BSS node_2 = (BSS) java.rmi.Naming.lookup("rmi://localhost/BSS_" + "2");
+
+
+            if(this.id == "1"){
+                node_2.send_message("message from" + this.id);
+            }else{
+                node_1.send_message("message from" + this.id);
+            }
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
-    @Override
+        @Override
     public void send_message(String text) throws RemoteException {
-        System.out.print(text);
+        System.out.print( "I, " + this.id + "Have received: "+ text );
     }
 }
