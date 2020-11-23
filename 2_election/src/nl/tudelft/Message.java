4438476d5c27e44c7a12e0793ed2cb2d313b678c
Ryudas <sivlesegro9@gmail.com>
2020-11-23 13:44:25 +0100

    Initial peterson election files

---
+++ 2_election/src/nl/tudelft/Message.java
@@ -0,0 +1,26 @@
+package nl.tudelft;
+
+import java.io.Serializable;
+
+/**
+ * This is the message being sent between nodes.
+ *
+ */
+public class Message implements Serializable {
+  public int [] clock;
+  public String sender;
+
+
+  public Message(int[] clock, String sender) {
+    this.clock = new int[3];
+    this.clock = clock.clone();
+    this.sender = sender;
+  }
+
+  public void print() {
+    System.out.println("This is the vector clock:");
+    for (int j : this.clock) {
+      System.out.println(j);
+    }
+  }
+}
