cd75da21343a15875d5c015eca6045832b61caac
cholland <c.r.holland@student.tudelft.nl>
2020-11-24 18:04:09 +0100

    Finish the peterson algorithm

--- 2_election/src/nl/tudelft/Message.java
+++ 2_election/src/nl/tudelft/Message.java
@@ -7,20 +7,13 @@ import java.io.Serializable;
  *
  */
 public class Message implements Serializable {
-  public int [] clock;
-  public String sender;
+  public int id;
+  public String type;
 
 
-  public Message(int[] clock, String sender) {
-    this.clock = new int[3];
-    this.clock = clock.clone();
-    this.sender = sender;
+  public Message(int id, String type) {
+    this.id = id;
+    this.type = type;
   }
 
-  public void print() {
-    System.out.println("This is the vector clock:");
-    for (int j : this.clock) {
-      System.out.println(j);
-    }
-  }
 }
