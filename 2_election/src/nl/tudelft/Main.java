ec6306349e0d862fbecb964526b28699018b1ba8
cholland <c.r.holland@student.tudelft.nl>
2020-11-24 18:04:09 +0100

    Finish the peterson algorithm

--- 2_election/src/nl/tudelft/Main.java
+++ 2_election/src/nl/tudelft/Main.java
@@ -8,10 +8,6 @@ import java.rmi.registry.Registry;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-//        System.out.print("LALA");
-
-
 
         // Initialize registry
         try {
@@ -19,43 +15,51 @@ public class Main {
             java.rmi.registry.LocateRegistry.createRegistry(1098);
             Registry registry = LocateRegistry.getRegistry(1098);
 
-            Peterson obj1 = new Peterson("1");
-            int[] sched = new int[1];
-            obj1.set_schedule(sched);
-            Peterson obj2 = new Peterson("2");
-            sched = new int[1];
-            obj2.set_schedule(sched);
-            sched = new int[1];
-            Peterson obj3 = new Peterson("3");
-            obj3.set_schedule(sched);
+            Peterson obj1 = new Peterson("4", 7);
+            Peterson obj2 = new Peterson("9", 4);
+            Peterson obj3 = new Peterson("12", 9);
+            Peterson obj4 = new Peterson("1", 12);
+            Peterson obj5 = new Peterson("14", 1);
+            Peterson obj6 = new Peterson("7", 14);
+//            Peterson obj6 = new Peterson("8", 3);
+//            Peterson obj7 = new Peterson("2", 8);
+//            Peterson obj8 = new Peterson("6", 2);
+//            Peterson obj9 = new Peterson("5", 6);
+//            Peterson obj10 = new Peterson("7", 5);
+
+
 
             // Create threads for execution
             Thread t_1 = new Thread(obj1);
             Thread t_2 = new Thread(obj2);
             Thread t_3 = new Thread(obj3);
+            Thread t_4 = new Thread(obj4);
+            Thread t_5 = new Thread(obj5);
+            Thread t_6 = new Thread(obj6);
+//            Thread t_7 = new Thread(obj7);
+//            Thread t_8 = new Thread(obj8);
+//            Thread t_9 = new Thread(obj9);
+//            Thread t_10 = new Thread(obj10);
 
             // Start all objects in parallel
             t_1.start();
             t_2.start();
             t_3.start();
-            /*
-            BSS obj1 = new BSS("1");
-            BSS obj2 = new BSS("2");
-            BSS obj3 = new BSS("3");
-            BSS_RMI newobj = (BSS_RMI) registry.lookup("BSS-1");
-            int[] clock = new int[3];
-            Message m1 = new Message(clock, "3");
-            obj3.broadcast(m1);
-            obj2.broadcast(new Message(obj2.getClock(),"2"));
-
+            t_4.start();
+            t_5.start();
+            t_6.start();
+//            t_7.start();
+//            t_8.start();
+//            t_9.start();
+//            t_10.start();
 
             //Print the data after the scenario
-            obj1.printData();
-            obj2.printData();
-            obj3.printData();
+//            obj1.printData();
+//            obj2.printData();
+//            obj3.printData();
 
 
-             */
+
         } catch (RemoteException e ) {
             e.printStackTrace();
         } catch (MalformedURLException e) {
--- 2_election/src/nl/tudelft/Peterson.java
+++ 2_election/src/nl/tudelft/Peterson.java
@@ -7,62 +7,50 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.ArrayList;
 import java.util.Arrays;
 
-// Birman-Schiper-Stephenson algorithm class
+// Peterson algorithm class
 public class Peterson extends UnicastRemoteObject implements Peterson_RMI, Runnable  {
-    private String id;
-    private int[] clock;
-    private ArrayList<Message> buffer;
-    private int[] schedule;
+    private int tid;
+    private int ntid;
+    private int nntid;
+    private boolean passive;
+    private boolean elected;
+    private String neighbour;
+    private boolean receivedNtid;
+    private boolean receivedNntid;
+
 
     // constructor for a component of BSS
-    protected Peterson(String id) throws RemoteException, AlreadyBoundException, MalformedURLException {
+    protected Peterson(String neighbour, int tid) throws RemoteException, AlreadyBoundException, MalformedURLException {
         // create a BSS component with a certain id and bind it to the remote
-        this.id = id;
-        this.buffer = new ArrayList<>();
-        this.clock = new int[3];
-        java.rmi.Naming.bind("rmi://localhost:1098/BSS-" + id.toString(), this);
+        this.tid = tid;
+//        this.ntid = -1;
+//        this.nntid = -1;
+        this.passive = false;
+        this.elected = false;
+        this.receivedNtid = false;
+        this.receivedNntid = false;
+        this.neighbour = neighbour;
+        java.rmi.Naming.bind("rmi://localhost:1098/Peterson-" + tid, this);
 
     }
 
-    //
-    public void set_schedule(int[] msg_times){
-        this.schedule = msg_times;
 
-    }
     @Override
     public void run() {
-        // dummy amounts now
-        for( int i = 0 ; i < this.schedule.length; i++){
-            // if clock is not current to the schedule get it to it,
-            // create message from current elem to all others
-            this.incrementClock();
-            Message m1 = new Message(this.clock, this.id);
-            broadcast(m1);
-        }
-
+        this.send(new Message(this.tid,"ntid"));
     }
 
-    public void broadcast(Message message) {
+    public void send(Message message) {
         Registry registry = null;
         try {
             // load all objects
             registry = LocateRegistry.getRegistry(1098);
-            Peterson_RMI one = (Peterson_RMI) registry.lookup("BSS-1");
-            Peterson_RMI two = (Peterson_RMI) registry.lookup("BSS-2");
-            Peterson_RMI three = (Peterson_RMI) registry.lookup("BSS-3");
+            Peterson_RMI neighbour = (Peterson_RMI) registry.lookup("Peterson-"+this.neighbour);
+            delay();
+            neighbour.receive(message);
 
-           // this.incrementClock();
-           // message.clock[Integer.parseInt(message.sender)-1]++;
-            // process random delay
-            delay();
-            one.receive(message);
-            delay();
-            two.receive(message);
-            delay();
-            three.receive(message);
         } catch (RemoteException e) {
             e.printStackTrace();
         } catch (NotBoundException e) {
@@ -74,70 +62,94 @@ public class Peterson extends UnicastRemoteObject implements Peterson_RMI, Runna
     }
 
 
-    /**
-     * I think we shouldd change a few things in our algorithm, for example vplusej is up to date with the message
-     * sender clock. Only increment if it is not him sending it to himself..
-     * If you check the example of: https://www.google.com/search?q=birman+schiper+stephenson+protocol+example&sxsrf=ALeKk03YA7SOToH5c_mp6Z5slfeg3vUuKQ:1605647563305&tbm=isch&source=iu&ictx=1&fir=Wz9WhUmMB_5CyM%252C9OLhdgKzv0apYM%252C_&vet=1&usg=AI4_-kSHrVxQMSCeATrP0v9ROlXjDoCidw&sa=X&ved=2ahUKEwibpJSlv4rtAhVS1xoKHdT6B9gQ9QF6BAgIEC8#imgrc=X3ZFXsVUs_dtiM
-     * I used that example in the main, and the values seem to be the same if I use compareLT.
-     * We also just need to make sure we run them in threads and that our buffer works.
-     * Our compare doesn't work like it should.
-     * @param message
-     * @throws RemoteException
-     */
+
     @Override
-    public void receive(Message message) throws RemoteException {
-        System.out.println(this.id +" received the message w/ clock" + Arrays.toString(message.clock) + " from " + message.sender);
-        int [] vplusej = this.clock;
-        //if (!this.id.equals(message.sender)) {
-            //Adding this and changing to compLT somehow works...
-            //this.clock[Integer.parseInt(message.sender)-1] = message.clock[Integer.parseInt(message.sender)-1];
+    public void receive(Message message) {
 
-            vplusej[Integer.parseInt(message.sender)-1] +=1;
-           // this.incrementClock();
-        //}
 
-        //System.out.println("vplusej: "+ Arrays.toString(vplusej) +". message clock: " + Arrays.toString(message.clock) +"" +
-        //        ". vplusej is larger than message.clock: " + compGET(vplusej, message.clock));
-
-        if (compGET(vplusej, message.clock)) {
-            deliver(message);
-
-            // once we delivered we might deliver more from the buffer
-            this.buffer.forEach((Vm) -> {
-                int [] vplus_ek = this.clock;
-                vplus_ek[Integer.parseInt(Vm.sender)-1] += 1;
-                if(compGET(vplus_ek, Vm.clock)) {
-                    //deliver the message or deliver Vm?
-                    deliver(Vm);
-                }
-            });
+        if (message.type.equals("elected") || this.tid == message.id) {
+          System.out.println("Finished with elected: " + message.id);
+          return;
         }
+
+        if (this.passive) {
+          System.out.println("[" + this.tid + "] " + "[" + message.type + "] Relaying: " + message.id);
+             this.relay(message);
+         }
         else {
-            this.buffer.add(message);
-            System.out.println("Message from "+ message.sender+  " with clock: " + Arrays.toString(message.clock) + " added to the buffer of: " +  this.id );
+            System.out.println("[" + this.tid + "] " + "[" + message.type + "] Message received: " + message.id);
+
+
+            if (message.type.equals("ntid")) {
+              this.ntid = message.id;
+              this.receivedNtid = true;
+              receiveNtid(message);
+            }
+
+
+            if (message.type.equals("nntid")) {
+              this.nntid = message.id;
+              this.receivedNntid = true;
+              receiveNntid(message);
+            }
+
+
+            if (this.ntid >= this.tid && this.ntid >= this.nntid) {
+                System.out.println(this.tid + " has been changed to " + this.ntid);
+                this.tid = this.ntid;
+                this.passive = false;
+            }
+            else {
+                this.passive = true;
+                System.out.println("I have been idled!");
+            }
+            this.receivedNntid = false;
+            this.receivedNtid = false;
+            send(new Message(this.tid, "ntid"));
         }
-    }
-
-    /**
-     * Optional change?: instead of incrementing by 1, set it to the clock value itself,
-     * so it will work if it is two ahead.
-     * @param message
-     */
-    public void deliver(Message message) {
-//        this.incrementClock();
-        //if (!this.id.equals(message.sender)){
-            this.clock[Integer.parseInt(message.sender)-1] += 1;
-            this.buffer.remove(message);
-            System.out.println("Message delivered from " + message.sender + "in process:" + this.id  + Arrays.toString(message.clock));
-            System.out.println();
-            System.out.println(this.id + " now has the clock: " + Arrays.toString(this.clock));
-
-       // }
 
     }
+
+    public void relay(Message message) {
+        if (this.tid == message.id) {
+            this.elected = true;
+        }
+        this.send(message);
+    }
+
+    public void receiveNtid(Message message) {
+      while (!receivedNtid) {
+        try {
+          Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+      if (this.tid == this.ntid) {
+        this.elected = true;
+        send(new Message(this.tid, "elected"));
+      }
+      send(new Message(Math.max(this.tid, this.ntid), "nntid"));
+
+    }
+
+  public void receiveNntid(Message message) {
+    while (!receivedNntid) {
+      try {
+        Thread.sleep(1000L);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+    if (this.tid == this.nntid) {
+      this.elected = true;
+      send(new Message(this.tid, "elected"));
+    }
+
+  }
 
     public void delay() {
-        int delay = (int) (Math.random()*10);
+      int delay = (int) (Math.random()*2);
 //        System.out.println(delay);
         try {
             Thread.sleep(delay*1000L);
@@ -147,54 +159,10 @@ public class Peterson extends UnicastRemoteObject implements Peterson_RMI, Runna
     }
 
     public void printData() {
-        System.out.println("This is the vector clock for BSS" + this.id + ":");
-        System.out.print("[ ");
-        for (int i = 0; i < this.clock.length; i++) {
-            System.out.print(this.clock[i] + " ");
-        }
-        System.out.print("]");
-        System.out.println();
-        System.out.println("This is the buffer for BSS" + this.id + ": ");
-        this.buffer.forEach( (arr) ->
-                System.out.println(Arrays.toString(arr.clock)));
+        System.out.println("tid: " + this.tid + ", ntid: " + this.ntid + ", nntid: " + this.nntid);
     }
 
-    public void incrementClock() {
-        this.clock[Integer.parseInt(this.id)-1]++;
-    }
 
-    public boolean compGET(int[] first, int[] second) {
-        //System.out.println("Compare get:");
-        //System.out.println(Arrays.toString(first));
-        //System.out.println(Arrays.toString(second));
-        //System.out.println();
-        for (int i = 0; i < 3; i++) {
-            //System.out.println("Enter the for loop");
-            if (first[i] < second[i]) {
-               // System.out.println("first is smaller than second for element " + i);
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public boolean compLT(int[] first, int[] second) {
-        for (int i = 0; i < 3; i++) {
-            if (first[i] >= second[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public int[] getClock() {
-        return this.clock;
-    }
-
-    public ArrayList<Message> getBuffer() {
-        return buffer;
-    }
 
     //    public boolean getNextFromBuffer() {
 //        System.out.println(this.buffer.stream().findAny().get());
